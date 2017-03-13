29ef3963eda3a0ae2a12fee72bfb3e9bd4f92043
dfreemanRIIS <dfreeman@riis.com>
2017-03-13 11:06:25 -0400

    Finish ETL

--- swift/etl/Sources/Etl.swift
+++ swift/etl/Sources/Etl.swift
@@ -1,5 +1,15 @@
 class ETL {
-	func transform(old:[Int]) -> Int {
-		return 1
+	static func transform(_ old:[Int:[String]]) -> [String:Int] {
+		var dictionary:[String:Int] = [:]
+		for key in old.keys {
+			for letter in old[key]! {
+				let temp = letter.lowercased()
+				dictionary[temp] = key
+			}
+		}
+		if (dictionary != nil) {
+			return dictionary
+		}
+		return[:]
 	}
 }
